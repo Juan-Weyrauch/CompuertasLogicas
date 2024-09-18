c79aedbac5594c64ef26566d196dcec83d1c1d92
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2024-09-18 20:00:17 -0300

    inicio de tests

--- TestProject/AndTests.cs
+++ TestProject/AndTests.cs
@@ -12,19 +12,56 @@ public class AndTests
     [SetUp]
     public void Setup()
     {
-        // Arrange
-        var input = new List<bool> { true, true };
+        _and = new And();
+    }
 
-        // Act
-        var result = _and.Funcion(input);
+    [Test]
+    public void If_Both_True_Then_Return_True()
+    {
+        List<bool> input = new List<bool> { true, true };
 
-        // Assert
+        bool result = _and.Funcion(input);
+
         Assert.IsTrue(result);
     }
 
     [Test]
-    public void Test1()
+    public void Funcion_OneFalse_ReturnsFalse()
     {
-        Assert.Pass();
+        List<bool> input = new List<bool> { true, false };
+
+        bool result = _and.Funcion(input);
+
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Funcion_BothFalse_ReturnsFalse()
+    {
+        List<bool> input = new List<bool> { false, false };
+
+        bool result = _and.Funcion(input);
+
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Funcion_OnlyUsesFirstTwoElements()
+    {
+        List<bool> input = new List<bool> { true, false, false };
+
+        bool result = _and.Funcion(input);
+
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Funcion_EmptyList_OutOfRange()
+    {
+        List<bool> input = new List<bool>();
+
+        Assert.Throws<System.ArgumentOutOfRangeException>(() => _and.Funcion(input));
+        
+        // using _and.Funcion(input) we expect to show the exception System.ArgumentOutOfRange ggs
     }
 }
--- TestProject/NotTests.cs
+++ TestProject/NotTests.cs
@@ -1,6 +1,28 @@
+using System;
+using Library; 
 namespace TestProject;
 
+[TestFixture]
 public class NotTests
 {
+    private Not _or;
+
+    [SetUp]
+
+    public void Setup()
+    {
+        _or = new Not();
+    }
+
+    [Test]
+    public void If_False_Return_True()
+    {
+        bool input = false;
+        bool expected = _or.Funcion(input);
+        
+        Assert.IsTrue(expected);
+    }
     
+    
+        
 }
