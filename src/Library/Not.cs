30283eae98f4992e3d16d2a5d2699ca71a7be042
J <juanma.lorenzo@correo.ucu.edu.uy>
2024-09-18 19:46:34 -0400

    Creacion de el programa de la compuerta

--- src/Library/Not.cs
+++ src/Library/Not.cs
@@ -1,6 +1,5 @@
 using Library;
 
-namespace DefaultNamespace;
 
 public class Not: IPuertaLogica
 {
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,18 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using DefaultNamespace;
 using Library;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var and = new And();
+        /*var and = new And();
         var or = new Or();
         var not = new Not();
         Console.WriteLine(and.Funcion([false, true]));
         Console.WriteLine(or.Funcion([false, true]));
         Console.WriteLine(not.Funcion([false]));
-
+        */
+        
+        GarageGate compuerta = new GarageGate();
+        Console.WriteLine(compuerta.Funcion([true,false,false]));
+        // El [0] de la lista es el a, el [1] es el b, [2] es el c.
     }
 }
